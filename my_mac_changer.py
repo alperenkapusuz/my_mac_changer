391e65366d86fd908937ac767910e4ebb084351a
Alperen Kapusuz <63202119+alperenkapusuz@users.noreply.github.com>
2022-01-17 18:03:05 +0300

    added function version

--- my_mac_changer.py
+++ my_mac_changer.py
@@ -1,21 +1,23 @@
 import subprocess
 import optparse
 
-parse_object = optparse.OptionParser()
-parse_object.add_option("-i","--interface",dest="interface",help="interface to change!")
-parse_object.add_option("-m","--mac",dest="mac_address",help="new mac address")
+def get_user_input():
+    parse_object = optparse.OptionParser()
+    parse_object.add_option("-i","--interface",dest="interface",help="interface to change!")
+    parse_object.add_option("-m","--mac",dest="mac_address",help="new mac address")
 
-(user_inputs, arguments) = parse_object.parse_args()
+    return parse_object.parse_args()
 
-user_interface = user_inputs.interface
-user_mac_address = user_inputs.mac_address
+def change_mac_address(user_interface,user_mac_address):
+    subprocess.call(["ifconfig",user_interface,"down"])
+    subprocess.call(["ifconfig",user_interface,"hw","ether",user_mac_address])
+    subprocess.call(["ifconfig",user_interface,"up"])
+
+def control_new_mac(interface):
+    ifconfig = subprocess.check_output(["ifconfig",interface])
+    print(ifconfig)
 
 print("my mac changer started!")
-
-#interface = "eth0"
-#mac_addres = "00:22:33:77:99:11"
-
-subprocess.call(["ifconfig",user_interface,"down"])
-subprocess.call(["ifconfig",user_interface,"hw","ether",user_mac_address])
-subprocess.call(["ifconfig",user_interface,"up"])
-
+(user_input,arguments) = get_user_input()
+change_mac_address(user_input.interface, user_input.mac_address)
+control_new_mac(user_input.interface)
